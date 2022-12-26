60fd3e593a822c97ae4cbeb5a12ca1f8c2354336
Prakhar Sharma <prakharentp2002@gmail.com>
2022-12-26 21:02:47 +0530

    Contract written

---
+++ contracts/FakeNFT.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract FakeNFT is ERC721{
+    constructor() ERC721("FakeNFT", "FNFT"){
+
+    }
+
+    uint256 tokenId = 1;
+    uint256 constant price = 0.01 ether;
+
+    function mint() public payable {
+        require(msg.value == price,"Price Incorrect!");
+        _mint(msg.sender, tokenId);
+        tokenId += 1;
+    }
+}
